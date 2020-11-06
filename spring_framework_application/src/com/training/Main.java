3fab21b2c2b5191aed28db9d4204609a593ac963
pavan.reddy <saipavanreddyr21@gmail.com>
2020-11-06 12:34:44 +0530

    spring-framework commit 1

---
+++ spring_framework_application/src/com/training/Main.java
@@ -0,0 +1,26 @@
+package com.training;
+
+
+import org.springframework.beans.factory.BeanFactory;
+import org.springframework.beans.factory.xml.XmlBeanFactory;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.core.io.Resource;
+
+public class Main {
+	public static void main(String[] args) {
+		
+//		Resource resource=new ClassPathResource("applicationContext.xml");
+//		BeanFactory factory=new XmlBeanFactory(resource);
+		
+		ApplicationContext context = new ClassPathXmlApplicationContext("applicationContext.xml");
+		Employee e1=(Employee)context.getBean("empBean");
+		Address a1 = (Address)context.getBean("addBean");
+		System.out.println(e1);
+		System.out.println(a1);
+		
+		
+	}
+
+} 
